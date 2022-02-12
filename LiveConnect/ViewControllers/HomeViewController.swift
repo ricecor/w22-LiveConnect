ed4592ca00a1fb0d1f0ca67f9f5f08883263d7f7
stoneyboi <stonej2@mail.gvsu.edu>
2022-02-11 21:15:07 -0500

    Initial Log In Screen composed

---
+++ LiveConnect/ViewControllers/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  LiveConnect
+//
+//  Created by Jake Stone on 2/11/22.
+// Home View Controller is the main screen that the user will see after logging in to the app
+// it will come from log in or sign up screen
+// and have Settings Button in corner for user preferences to be changed
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
