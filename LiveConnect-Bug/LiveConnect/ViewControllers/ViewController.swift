8be03912a0acfd961b992f37c698fb61dd4b45e0
Jacob Stone <70761885+stoneyboi@users.noreply.github.com>
2022-03-17 09:46:11 -0400

    added polling feature but still has problems

---
+++ LiveConnect-Bug/LiveConnect/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveConnect
+//
+//  Created by Jake Stone on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
